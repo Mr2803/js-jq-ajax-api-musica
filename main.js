3a7a134fc723256021620892c4cec30a5806ad63
Mr2803 <rispoli.mar2803@gmail.com>
2019-11-19 16:19:33 +0100

    finish exercise (basic)

--- main.js
+++ main.js
@@ -14,16 +14,45 @@ partite dalla questione logica e quindi andate di console.log;
 poi aggiungete la questione output handlebars. */
 
 $(document).ready(function() {
+	/* var source = $(".cell").html()
+	var template = Handlebars.compile(source)
+
+	for (var i = 0; i < 10; i++) {
+		$(".cds-container").append(template);
+	} */
+
+
+
 	$.ajax({
 		url: "https://flynn.boolean.careers/exercises/api/array/music",
 		method: "GET",
 		success: function (data) {
-			console.log(data.response)
+			console.log("questo è ciò che mi ritorna l'api per intero " , data.response)
 			for (var i = 0; i < data.response.length; i++) {
 
-				console.log(data.response[i].poster + " " + data.response[i].title + data.response[i].author + " " + data.response[i].genre + " " + data.response[i].year);
+			
+				//il source mi restituisce il div per intero che ho inserito nell'html
+				var source = $(".cd-global").text()
+				/* console.log(source) */
+				var template = Handlebars.compile(source)
+				/* console.log(template) */
+				/* var globalsong = {
+					img : data.response[i].poster,
+					title : data.response[i].title,
+					author : data.response[i].author,
+					genre : data.response[i].genre,
+					year : data.response[i].year
+				} */
+				/* console.log(globalsong) */
+				var html = template(data.response[i]);
+				console.log(data.response[i])
+				console.log(html)
+				$(".cds-container.container").append(html)
 			}
 			
+		},
+		error: function (richiesta, stato, errori) {
+			alert("Errore " + richiesta + stato + errori);
 		}
 	})
 });
